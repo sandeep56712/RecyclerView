55877486c3e499a8847f979eb3911cbc9a895a43
sandeep singh <sandeep56712@gmail.com>
2019-02-09 22:59:03 +0530

    first commit

---
+++ app/src/main/java/beangate/datta/recyclerv/Product.java
@@ -0,0 +1,67 @@
+package beangate.datta.recyclerv;
+
+public class Product {
+    private int id;
+    private String title;
+    private String shortdesc;
+    private double rating;
+    private double price;
+    private String image;
+    public Product(int id, String title, String shortdesc, double rating, double price, String image) {
+        this.id = id;
+        this.title = title;
+        this.shortdesc = shortdesc;
+        this.rating = rating;
+        this.price = price;
+        this.image = image;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getShortdesc() {
+        return shortdesc;
+    }
+
+    public void setShortdesc(String shortdesc) {
+        this.shortdesc = shortdesc;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+
+}
